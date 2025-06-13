358258e676e6f9d48456acdcdb015cca1e63f7fa
RaihanAryoWicaksono <raihanaryo2003@gmail.com>
2025-06-13 17:38:42 +0700

    Update

---
+++ Pakaian-Solid-1201230010_RAIHAN/Pakaian-Solid-1201230013_OWED/Pakaianku-PakaianLib/PakaianLib/PakaianEnum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PakaianLib
+{
+    public enum StatusPakaian
+    {
+        Tersedia,         
+        DalamKeranjang,   
+        Dipesan,          
+        Dibayar,          
+        DalamPengiriman,  
+        Diterima,            
+        TidakTersedia     
+    }
+
+    
+    public enum AksiPakaian
+    {
+        TambahKeKeranjang,
+        KeluarkanDariKeranjang,
+        Pesan,
+        BatalPesan,
+        Bayar,
+        Kirim,
+        TerimaPakaian,
+        KembalikanPakaian,
+        RestokPakaian,
+        HabisStok,
+        Tersedia,
+        SelesaiCheckout
+    }
+}
---
+++ Pakaian-Solid-1201230010_RAIHAN/Pakaian-Solid-1201230013_OWED/PakaiankuGUI/PakaianForm/Panel/panelCariPakaian.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PakaianForm.Panel
+{
+    partial class panelCariPakaian
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelItem1 = new PakaianForm.Panel.panelItem();
+            this.panelItem2 = new PakaianForm.Panel.panelItem();
+            this.panelItem3 = new PakaianForm.Panel.panelItem();
+            this.panelItem4 = new PakaianForm.Panel.panelItem();
+            this.panelItem5 = new PakaianForm.Panel.panelItem();
+            this.panelItem6 = new PakaianForm.Panel.panelItem();
+            this.panelItem7 = new PakaianForm.Panel.panelItem();
+            this.panelItem8 = new PakaianForm.Panel.panelItem();
+            this.panelItem9 = new PakaianForm.Panel.panelItem();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.27826F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cari Pakaian";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(125, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(676, 33);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.01739F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cari : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 10.01739F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(820, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.panelItem1);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem2);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem3);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem4);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem5);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem6);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem7);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem8);
+            this.flowLayoutPanel1.Controls.Add(this.panelItem9);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 188);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(961, 422);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // panelItem1
+            // 
+            this.panelItem1.Location = new System.Drawing.Point(3, 3);
+            this.panelItem1.Name = "panelItem1";
+            this.panelItem1.Size = new System.Drawing.Size(251, 294);
+            this.panelItem1.TabIndex = 0;
+            // 
+            // panelItem2
+            // 
+            this.panelItem2.Location = new System.Drawing.Point(260, 3);
+            this.panelItem2.Name = "panelItem2";
+            this.panelItem2.Size = new System.Drawing.Size(251, 294);
+            this.panelItem2.TabIndex = 1;
+            // 
+            // panelItem3
+            // 
+            this.panelItem3.Location = new System.Drawing.Point(517, 3);
+            this.panelItem3.Name = "panelItem3";
+            this.panelItem3.Size = new System.Drawing.Size(251, 294);
+            this.panelItem3.TabIndex = 2;
+            // 
+            // panelItem4
+            // 
+            this.panelItem4.Location = new System.Drawing.Point(3, 303);
+            this.panelItem4.Name = "panelItem4";
+            this.panelItem4.Size = new System.Drawing.Size(251, 294);
+            this.panelItem4.TabIndex = 3;
+            // 
+            // panelItem5
+            // 
+            this.panelItem5.Location = new System.Drawing.Point(260, 303);
+            this.panelItem5.Name = "panelItem5";
+            this.panelItem5.Size = new System.Drawing.Size(251, 294);
+            this.panelItem5.TabIndex = 4;
+            // 
+            // panelItem6
+            // 
+            this.panelItem6.Location = new System.Drawing.Point(517, 303);
+            this.panelItem6.Name = "panelItem6";
+            this.panelItem6.Size = new System.Drawing.Size(251, 294);
+            this.panelItem6.TabIndex = 5;
+            // 
+            // panelItem7
+            // 
+            this.panelItem7.Location = new System.Drawing.Point(3, 603);
+            this.panelItem7.Name = "panelItem7";
+            this.panelItem7.Size = new System.Drawing.Size(251, 294);
+            this.panelItem7.TabIndex = 6;
+            // 
+            // panelItem8
+            // 
+            this.panelItem8.Location = new System.Drawing.Point(260, 603);
+            this.panelItem8.Name = "panelItem8";
+            this.panelItem8.Size = new System.Drawing.Size(251, 294);
+            this.panelItem8.TabIndex = 7;
+            // 
+            // panelItem9
+            // 
+            this.panelItem9.Location = new System.Drawing.Point(517, 603);
+            this.panelItem9.Name = "panelItem9";
+            this.panelItem9.Size = new System.Drawing.Size(251, 294);
+            this.panelItem9.TabIndex = 8;
+            // 
+            // panelCariPakaian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "panelCariPakaian";
+            this.Size = new System.Drawing.Size(967, 613);
+            this.Load += new System.EventHandler(this.panelCariPakaian_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private panelItem panelItem1;
+        private panelItem panelItem2;
+        private panelItem panelItem3;
+        private panelItem panelItem4;
+        private panelItem panelItem5;
+        private panelItem panelItem6;
+        private panelItem panelItem7;
+        private panelItem panelItem8;
+        private panelItem panelItem9;
+    }
+}
