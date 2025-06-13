cadf58fc7fbb9ed1c9bbf5179b6443e41c51e51f
AweDNur <nurmwahyudewata@gmail.com>
2025-06-13 16:56:37 +0700

    uji coba GUI

---
+++ PakaiankuGUI/PakaianForm/Panel/panelCariPakaian.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PakaianForm.Panel
+{
+    public partial class panelCariPakaian: UserControl
+    {
+        public panelCariPakaian()
+        {
+            InitializeComponent();
+        }
+
+        private void panelCariPakaian_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
