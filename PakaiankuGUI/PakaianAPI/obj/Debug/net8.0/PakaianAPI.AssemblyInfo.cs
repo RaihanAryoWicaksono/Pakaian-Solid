98a12ba3fc8de1465a0e324f7bad57329e3bb02f
Dimas Iqbal <dimasiqbal888@gmail.com>
2025-06-16 03:21:24 +0700

    add untrack file

--- PakaiankuGUI/PakaianAPI/obj/Debug/net8.0/PakaianAPI.AssemblyInfo.cs
+++ PakaiankuGUI/PakaianAPI/obj/Debug/net8.0/PakaianAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PakaianAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae19283939d9d8fa923daa20f848435cc2a5172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccc07166fa0e0a77bfdeff4a8e9566f3e1f5dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("PakaianAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PakaianAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
