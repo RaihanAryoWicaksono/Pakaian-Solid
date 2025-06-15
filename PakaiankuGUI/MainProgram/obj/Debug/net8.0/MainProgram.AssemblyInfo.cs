df7a1a53bfab8099a430f647e29f94bd63d987e0
Dimas Iqbal <dimasiqbal888@gmail.com>
2025-06-16 03:08:45 +0700

    simpan perubahan pakaianlib

--- PakaiankuGUI/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
+++ PakaiankuGUI/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2464cb01dfd331c79f83ba70041d0e3db692a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8a5a01facb17162b05ee5936afbd96422f1053")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
