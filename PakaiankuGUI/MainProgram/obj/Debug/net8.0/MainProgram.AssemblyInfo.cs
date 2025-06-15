dd56988abad7622d29761f636f9569eb7cb64056
Dimas Iqbal <dimasiqbal888@gmail.com>
2025-06-16 02:50:00 +0700

    delete pakaian lib

--- PakaiankuGUI/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
+++ PakaiankuGUI/MainProgram/obj/Debug/net8.0/MainProgram.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6f23809de962667c030d0150af4064af99e8f1")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa99577e0a7e619125ddf4eebeac25ee53e3d703")]
->>>>>>> 1201230009_maulana
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2464cb01dfd331c79f83ba70041d0e3db692a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
